39667289ea41f684032843287dd39e98a01b97ea
Philip Offtermatt <p.offtermatt@gmail.com>
2023-05-05 13:52:28 +0200

    Remove Block and start by adding ABCIQuery

--- src/rpc_server/routes.go
+++ src/rpc_server/routes.go
@@ -1,26 +1,20 @@
 package rpc_server
 
 import (
+	"github.com/cometbft/cometbft/libs/bytes"
 	ctypes "github.com/cometbft/cometbft/rpc/core/types"
 	rpc "github.com/cometbft/cometbft/rpc/jsonrpc/server"
 	rpctypes "github.com/cometbft/cometbft/rpc/jsonrpc/types"
 )
 
 var Routes = map[string]*rpc.RPCFunc{
-	// info API
-	"block": rpc.NewRPCFunc(Block, "height", rpc.Cacheable("height")),
-
 	// // tx broadcast API
 	// "broadcast_tx_commit": rpc.NewRPCFunc(BroadcastTxCommit, "tx"),
 	// "broadcast_tx_sync":   rpc.NewRPCFunc(BroadcastTxSync, "tx"),
 	// "broadcast_tx_async":  rpc.NewRPCFunc(BroadcastTxAsync, "tx"),
 
 	// // abci API
-	// "abci_query": rpc.NewRPCFunc(ABCIQuery, "path,data,height,prove"),
-}
-
-func Block(ctx *rpctypes.Context, heightPtr *int64) (*ctypes.ResultBlock, error) {
-	return nil, nil
+	"abci_query": rpc.NewRPCFunc(ABCIQuery, "path,data,height,prove"),
 }
 
 // func BroadcastTxCommit(ctx *rpctypes.Context, tx types.Tx) (*ctypes.ResultBroadcastTxCommit, error) {
@@ -32,11 +26,11 @@ func Block(ctx *rpctypes.Context, heightPtr *int64) (*ctypes.ResultBlock, error)
 // func BroadcastTxAsync(ctx *rpctypes.Context, tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
 // }
 
-// func ABCIQuery(
-// 	ctx *rpctypes.Context,
-// 	path string,
-// 	data bytes.HexBytes,
-// 	height int64,
-// 	prove bool,
-// ) (*ctypes.ResultABCIQuery, error) {
-// }
+func ABCIQuery(
+	ctx *rpctypes.Context,
+	path string,
+	data bytes.HexBytes,
+	height int64,
+	prove bool,
+) (*ctypes.ResultABCIQuery, error) {
+}
