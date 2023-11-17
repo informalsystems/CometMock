b5f6151b2e21fdd7be581abff2197b4b4587d819
Philip Offtermatt <p.offtermatt@gmail.com>
2023-11-17 10:56:02 +0100

    Add script to restart testnet from beginning

---
+++ e2e-tests/local-testnet-singlechain-restart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# After the testnet was started, this script can restart it.
+# It does so by killing the existing testnet,
+# overwriting the node home directories with backups made
+# right after initializatio, and then starting the testnet again.
+
+
+BINARY_NAME=$1
+
+set -eux
+
+ROOT_DIR=${HOME}/nodes/provider
+BACKUP_DIR=${ROOT_DIR}_bkup
+
+if [ -z "$BINARY_NAME" ]; then
+    echo "Usage: $0 <binary_name> [cometmock_args]"
+    exit 1
+fi
+
+# Kill the testnet
+# pkill -f ^$BINARY_NAME &> /dev/null || true
+pkill -f ^cometmock &> /dev/null || true
+
+# Restore the backup
+rm -rf ${ROOT_DIR}
+cp -r ${BACKUP_DIR} ${ROOT_DIR}
